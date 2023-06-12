a954d8ba274588a46849d4812d75f2b110c9bf7a
Marzzio Chicana Romero <89101069+marzzioChicana@users.noreply.github.com>
2023-06-12 00:52:40 -0500

    Upload routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,16 @@ import { LoginSessionComponent } from './components/login-session/login-session.
 import { CheckInComponent } from './components/check-in/check-in.component';
 import { PaymentMethodComponent } from './components/payment-method/payment-method.component';
 import { ProductsComponent } from './components/products/products.component';
+import { ChatboxComponent } from './components/chatbox/chatbox.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+import { CarritoComponent } from './components/carrito/carrito.component';
+import { PurchaseHistoryComponent } from './components/purchase-history/purchase-history.component';
+import { CheckInCompanyComponent } from './components/check-in-company/check-in-company.component';
+import { ConfirmPurchaseComponent } from './components/confirm-purchase/confirm-purchase.component';
+import { CompraExitosaComponent } from './components/compra-exitosa/compra-exitosa.component';
 import { CerrarSesionComponent } from './components/cerrar-sesion/cerrar-sesion.component';
 import { ProfileViewComponent } from './components/profile-view/profile-view.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '', component: LandingComponent },
@@ -16,8 +24,15 @@ const routes: Routes = [
   { path: 'check/in', component: CheckInComponent },
   { path: 'payment/method', component: PaymentMethodComponent },
   { path: 'products', component: ProductsComponent },
-  { path: 'cerrar/sesion', component: CerrarSesionComponent},
-  { path: 'profile', component: ProfileViewComponent}
+  { path: 'chatbox', component: ChatboxComponent },
+  { path: 'edit/profile', component: EditProfileComponent },
+  { path: 'carrito', component: CarritoComponent },
+  { path: 'purchase/history', component: PurchaseHistoryComponent },
+  { path: 'check/in/company', component: CheckInCompanyComponent },
+  { path: 'confirm/purchase', component: ConfirmPurchaseComponent },
+  { path: 'purchase/successful', component: CompraExitosaComponent },
+  { path: 'close/session', component: CerrarSesionComponent },
+  { path: 'profile/view', component: ProfileViewComponent }
 ];
 
 @NgModule({
