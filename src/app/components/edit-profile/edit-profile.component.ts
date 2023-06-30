b984fcc634220f929d577ecb041e8735a0810acd
Marzzio Chicana Romero <89101069+marzzioChicana@users.noreply.github.com>
2023-06-30 13:16:25 -0500

    Update edit-profile.component.ts

--- src/app/components/edit-profile/edit-profile.component.ts
+++ src/app/components/edit-profile/edit-profile.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth-service/auth.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -9,81 +9,79 @@ import { UserService } from 'src/app/services/user.service';
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.css']
 })
-export class EditProfileComponent {
+export class EditProfileComponent implements OnInit {
   hide = true;
-  userForm!:FormGroup;
+  userForm!: FormGroup;
   userData!: any;
   id: number = 0;
-
-  variableDto: any 
+  variableDto: any;
 
   constructor(
-    private FormBuilder: FormBuilder,
+    private formBuilder: FormBuilder,
     private authService: AuthService,
     private userService: UserService,
     private router: Router
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.userData = this.authService.getUser();
 
-      if(this.userData?.lastName) {
-        this.generateReactiveFormCustomer();
+    if (this.userData?.lastName) {
+      this.generateReactiveFormCustomer();
 
-        this.userForm.patchValue({
-          nombre: this.userData?.name,
-          apellido: this.userData?.lastName,
-          direccion: this.userData?.address,
-          departamento: this.userData?.department,
-          distrito: this.userData?.district,
-          celular: this.userData?.phoneNumber,
-          password: this.userData?.password
-        })
-      } else if(this.userData?.ruc) {
-        this.generateReactiveFormCompany();
+      this.userForm.patchValue({
+        nombre: this.userData?.name,
+        apellido: this.userData?.lastName,
+        direccion: this.userData?.address,
+        departamento: this.userData?.department,
+        distrito: this.userData?.district,
+        celular: this.userData?.phoneNumber,
+        password: this.userData?.password
+      });
+    } else if (this.userData?.ruc) {
+      this.generateReactiveFormCompany();
 
-        this.userForm.patchValue({
-          nombre: this.userData?.name,
-          ruc: this.userData?.ruc,
-          direccion: this.userData?.address,
-          departamento: this.userData?.department,
-          distrito: this.userData?.district,
-          celular: this.userData?.phoneNumber,
-          password: this.userData?.password
-        })
-      } 
-  }
-
-  generateReactiveFormCustomer():void{
-    this.userForm = this.FormBuilder.group({
-      nombre:['', Validators.required],
-      apellido:['', Validators.required],
-      direccion:['', Validators.required],
-      departamento:[''],
-      distrito:[''],
-      celular:[''],
-      password:['', Validators.required],
-    })
-  }
-
-  generateReactiveFormCompany():void{
-    this.userForm = this.FormBuilder.group({
-      nombre:['', Validators.required],
-      ruc:['', Validators.required],
-      direccion:['', Validators.required],
-      departamento:[''],
-      distrito:[''],
-      celular:[''],
-      password:['', Validators.required],
-    })
-  }
-
-  updateUser(){
-    if (this.userForm.invalid){
-      console.log('Formulario Invalido')
+      this.userForm.patchValue({
+        nombre: this.userData?.name,
+        ruc: this.userData?.ruc,
+        direccion: this.userData?.address,
+        departamento: this.userData?.department,
+        distrito: this.userData?.district,
+        celular: this.userData?.phoneNumber,
+        password: this.userData?.password
+      });
     }
-    else {
-      if(this.userData?.lastName) {
+  }
+
+  generateReactiveFormCustomer(): void {
+    this.userForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      direccion: ['', Validators.required],
+      departamento: [''],
+      distrito: [''],
+      celular: [''],
+      password: ['', Validators.required]
+    });
+  }
+
+  generateReactiveFormCompany(): void {
+    this.userForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      ruc: ['', Validators.required],
+      direccion: ['', Validators.required],
+      departamento: [''],
+      distrito: [''],
+      celular: [''],
+      password: ['', Validators.required]
+    });
+  }
+
+  updateUser() {
+    if (this.userForm.invalid) {
+      console.log('Formulario Invalido');
+    } else {
+      if (this.userData?.lastName) {
         const user: any = {
           id: this.userData?.id,
           name: this.userForm.get('nombre')?.value,
@@ -96,15 +94,21 @@ export class EditProfileComponent {
           repeatPassword: this.userForm.get('password')?.value,
           email: this.userData?.email,
           points: this.userData?.points
-        }
-        this.userService.updateCustomer(user);
+        };
+        this.userService.updateCustomer(user).subscribe(
+          (data) => {
+            console.log(data);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
 
         this.cancelEdit();
 
         this.authService.setUser(user);
         console.log(this.authService.getUser());
-
-      } else if(this.userData?.ruc) {
+      } else if (this.userData?.ruc) {
         const user: any = {
           id: this.userData?.id,
           name: this.userForm.get('nombre')?.value,
@@ -116,8 +120,15 @@ export class EditProfileComponent {
           password: this.userForm.get('password')?.value,
           repeatPassword: this.userForm.get('password')?.value,
           email: this.userData?.email
-        }
-        this.userService.updateCompany(user);
+        };
+        this.userService.updateCompany(user).subscribe(
+          (data) => {
+            console.log(data);
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
 
         this.cancelEdit();
 
@@ -127,20 +138,18 @@ export class EditProfileComponent {
     }
   }
 
-  onSubmit(){
-   
-  }
+  onSubmit() {}
 
-  cancelEdit(){
+  cancelEdit() {
     this.router.navigate(['/edit/profile']);
     //this.location.back();
   }
 
-  viewCards(){
+  viewCards() {
     this.router.navigate(['/view/cards']);
   }
 
-  confirm(){
+  confirm() {
     this.updateUser();
     this.router.navigate(['/confirm/edit/profile']);
   }
@@ -148,7 +157,7 @@ export class EditProfileComponent {
   clickCancel(): void {
     const user = this.authService.getUser();
 
-    if(user?.lastName) {
+    if (user?.lastName) {
       this.router.navigate(['/products']);
     } else if (user?.ruc) {
       this.router.navigate(['/view/products']);
