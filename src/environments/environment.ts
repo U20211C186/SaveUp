bbc7fa8499bde22f71a98612669e403931dcbf96
Marzzio Chicana Romero <89101069+marzzioChicana@users.noreply.github.com>
2023-06-30 01:47:30 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    //baseURL:"https://saveup-api-production.up.railway.app/api/saveup/v1"
+    baseURL:"https://saveup-api-production.up.railway.app/api/saveup/v1"
 
-    baseURL: "http://localhost:8080/api/saveup/v1"
+    //baseURL: "http://localhost:8080/api/saveup/v1"
 };
